6ff0bf476ee478225b8c97282adfb72cf99923fd
Scott <sam4ku@virginia.edu>
2015-09-16 23:50:26 -0400

    First Milestone - gps doesn't work

--- app/src/main/java/edu/virginia/cs/uvathingstodo/MainActivity.java
+++ app/src/main/java/edu/virginia/cs/uvathingstodo/MainActivity.java
@@ -1,11 +1,14 @@
 package edu.virginia.cs.uvathingstodo;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,13 +17,56 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener{
+
+    protected GoogleApiClient mGoogleApiClient;
+
+    protected Location mLastLocation;
+
+    protected String mLatitudeText;
+    protected String mLongitudeText;
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        buildGoogleApiClient();
         setContentView(R.layout.activity_main);
+        mGoogleApiClient.connect();
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
     }
 
     @Override
@@ -78,5 +124,63 @@ public class MainActivity extends AppCompatActivity {
                 textView3.setText("You chose: " + selection3);
             break;
         }
+    }
+
+
+    public void getLocation(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_name);
+        Context context = getApplicationContext();
+        String name = editText.getText().toString();
+        int duration = Toast.LENGTH_SHORT;
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText = (String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText = (String.valueOf(mLastLocation.getLongitude()));
+        }
+
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (l != null) {
+                Toast.makeText(this, "Location is " + l.getLatitude() + " " + l.getLongitude(), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Location is null", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (SecurityException se) {
+
+        }
+
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText = (String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText = (String.valueOf(mLastLocation.getLongitude()));
+        } else {
+            //Toast.makeText(this, "No Location", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        //Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        //Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
